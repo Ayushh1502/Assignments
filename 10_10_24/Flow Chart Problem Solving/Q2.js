cfb0d44e4d2d448d56875707f143e3afc34274fe
Ayush <ayushrajput000031@gmail.com>
2024-10-13 08:11:04 +0530

    Area and perimeter Q

---
+++ 10_10_24/Flow Chart Problem Solving/Q2.js
@@ -0,0 +1,21 @@
+function check(L1,L2,B1,B2){
+    let A1 = L1 * B1;
+    let P1 = 2 + (L1 * B1);
+    let A2 = L2 * B2;
+    let P2 = 2 + (L2 * B2);
+
+    if(A1 > A2){
+        console.log("True");
+    }
+    else{
+        console.log("False");
+    }
+    if(P1 > P2){
+        console.log("True");
+    }
+    else{
+        console.log("False")
+    }
+}
+
+check(1,2,2,3);
