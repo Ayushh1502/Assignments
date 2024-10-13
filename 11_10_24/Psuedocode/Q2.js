9a30c7fd50f2104cdca9e5b823b68baefb7a275d
Ayush <ayushrajput000031@gmail.com>
2024-10-13 09:15:26 +0530

    solved Q2

--- 11_10_24/Psuedocode/Q2.js
+++ 11_10_24/Psuedocode/Q2.js
@@ -1,8 +1,31 @@
 // PSUEDOCODE
-// length1 = 1 , breadth1 = 2 , length2 = 2 , breadth2 = 3
 // area1 = length1 * breadth2
 // area2 = length2 * breadth2
 // perimeter1 = 2 * ( length1 + breadth1)
 // perimeter2 = 2 * (length2 + beradth2)
 // if area1 > area2 --> print true , else --> false
-// if perimeter1 > perimeter --> print true , else --> false
+// if perimeter1 > perimeter2 --> print true , else --> false
+
+// CODE
+let len1 = 1;
+let bre1 = 2;
+let len2 = 2;
+let bre2 = 3;
+
+let area1 = len1 * bre1;
+let per1 = 2 * (len1 + bre1);
+let area2 = len2 * bre2;
+let per2 = 2 * (len2 + bre2);
+
+if(area1 > area2){
+    console.log("True");
+}
+else{
+    console.log("False");
+}
+if(per1 > per2){
+    console.log("True");
+}
+else{
+    console.log("False");
+}
