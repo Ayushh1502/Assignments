cf888d571a79134f0f68936f96bcbf622fa5ea68
Ayush <ayushrajput000031@gmail.com>
2024-10-13 10:59:34 +0530

    explanation is added

--- 11_10_24/Psuedocode/Q2.js
+++ 11_10_24/Psuedocode/Q2.js
@@ -29,3 +29,14 @@ if(per1 > per2){
 else{
     console.log("False");
 }
+
+// EXPLANATION
+// here we have given length and breadth of both rectangles
+// firstly we have calculated area and perimeter of both rectangles
+// then we have used if else statement 
+// in the line 20 if the condition is true then it will print true if the conditon
+// is not true it will execute the else statement that is it will print false
+// again in the line 26 it is checking if the perimeter of rectangle 1 is bigger than
+// the rectangle 2 , if the condition is true then it will run the if statement and 
+// True will be print otherwise condition is false than else statement is exucted and 
+// False will be printed.
