8b5fce0ff42f75812636465342947a20a0b2af95
Ayush <ayushrajput000031@gmail.com>
2024-10-13 11:08:52 +0530

    solved Q3

---
+++ 11_10_24/Psuedocode/Q3.js
@@ -0,0 +1,20 @@
+// Pseudocode
+// give n 
+//  if n % 3 == 0 --> print Yes , else print No
+
+// code
+let n = 6;
+if(n % 3 == 0){
+    console.log("Yes");
+}
+else{
+    console.log("No");
+}
+
+// Explanation 
+// this is very simple code to check a number is divisble by 3 or not
+// we have given n = 6
+// in the line 7 we have used if condition to check the number is divisble by 
+// 3 or not if it is divisble by 3 then it will execute the if block and print Yes
+
+// otherwise it will execute the else block and print No
