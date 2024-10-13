447981d1a3e102fdab640a8bbdb00a0629aa0c87
Ayush <ayushrajput000031@gmail.com>
2024-10-13 09:08:11 +0530

    done

--- 10_10_24/Flow Chart Problem Solving/Q2.js
+++ 10_10_24/Flow Chart Problem Solving/Q2.js
@@ -1,8 +1,8 @@
 function check(L1,L2,B1,B2){
     let A1 = L1 * B1;
-    let P1 = 2 + (L1 * B1);
+    let P1 = 2 * (L1 + B1);
     let A2 = L2 * B2;
-    let P2 = 2 + (L2 * B2);
+    let P2 = 2 * (L2 + B2);
 
     if(A1 > A2){
         console.log("True");
---
+++ 11_10_24/Psuedocode/Q2.js
@@ -0,0 +1,8 @@
+// PSUEDOCODE
+// length1 = 1 , breadth1 = 2 , length2 = 2 , breadth2 = 3
+// area1 = length1 * breadth2
+// area2 = length2 * breadth2
+// perimeter1 = 2 * ( length1 + breadth1)
+// perimeter2 = 2 * (length2 + beradth2)
+// if area1 > area2 --> print true , else --> false
+// if perimeter1 > perimeter --> print true , else --> false
