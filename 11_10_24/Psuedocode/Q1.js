aafeca41c8539b582eedc5598a3ad705dc768077
Ayush <ayushrajput000031@gmail.com>
2024-10-13 08:56:12 +0530

    solved Q1

---
+++ 11_10_24/Psuedocode/Q1.js
@@ -0,0 +1,46 @@
+// PSUEDOCODE
+// We have two numbers num1 and num2
+// if num1 > num2 --> true , else --> false
+// if num1 < num2 --> true , else --> false
+// if num1 == num2 --> true , else --> false
+
+// CODE
+let num1 = 4;
+let num2 = 16;
+if(num1 > num2){
+    console.log("True");
+}
+else{
+    console.log("False");
+}
+if(num1 < num2){
+    console.log("True");
+}
+else{
+    console.log("False");
+}
+if(num1 == num2){
+    console.log("True");
+}
+else{
+    console.log("False");
+}
+
+
+// Explanation 
+
+// we have given num1 and num2
+// we have to print yes aur no accoring to using comparator operators
+// in the line 10 we have used if statement it says that if num1 is bigger than num2
+// then print yes
+// in the line 13 we are using else statement if the if statement is not executed
+// then it means that num1 is not bigger than num2 and it is printing False
+
+// then in the line 16 we are again checking if num1 is smaller than num2 ,if it is correct
+// it will print yes , if the if statement (line 16) is not executed then the else statement
+// is executed and it will print false
+
+// in the line 22 we are again checking if num1 is equal to num2 , if it is true then
+// it will execute the if statement and print True
+// is the if statement is false then it will execute the else statement 
+// and it will print false
