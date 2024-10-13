f115010203ef43acce79d329e4c14a5d111350b6
Ayush <ayushrajput000031@gmail.com>
2024-10-13 08:01:02 +0530

    inculeded functions

--- 10_10_24/Flow Chart Problem Solving/Q1.js
+++ 10_10_24/Flow Chart Problem Solving/Q1.js
@@ -1,21 +1,28 @@
-let num1 = 4;
-let num2 = 16;
+function tf(num1,num2){
+    let bag = "";
+    if(num1 > num2){
+        bag = bag + "True" + " ";
+    }
+    else{
+        bag = bag + "False" + " ";
+    }
+    if(num1 < num2){
+        bag = bag + "True" + " ";
+    }
+    else{
+        bag = bag + "False" + " ";
+    }
+    if(num1 == num2){
+        bag = bag + "True" + " ";
+    }
+    else{
+        bag = bag + "False" + " ";
+    }
 
-if(num1 > num2){
-    console.log("True");
+    return bag;
 }
-else{
-    console.log("False")
-}
-if(num1 < num2){
-    console.log("True");
-}
-else{
-    console.log("False");
-}
-if(num1 == num2){
-    console.log("True");
-}
-else{
-    console.log("False");
-}
+
+console.log(tf(4,16));
+console.log(tf(10,10));
+console.log(tf(8,2));
+
